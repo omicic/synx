3e57fb865624c399f13ad4b3cf281464b580c673
Olivera Micic <omicic@gmail.com>
2021-05-18 15:03:15 +0200

    pola

---
+++ index.php
@@ -0,0 +1,122 @@
+<?php include "partials/header.php"?>
+
+<section class="brands">
+    <article class="row align-items-center">
+        <div class="col-6">
+            <div class="wrapper p-5">
+                <p>always behind the brands</p>
+                <h5 class="pt-2">A studio helping web 2.0 gain some momentum.</h5>
+                <p class="py-3">The integration of web fonts has always been one of the largest contibuting
+                    factors to
+                    diversity in
+                    the overal look and feel of websites today vs. yestrday.</p>
+                <button class="btn btn-danger">Features</button>
+            </div>
+        </div>
+        <div class="col-6">
+            <img src="img/one.jpg" alt="">
+        </div>
+    </article>
+</section>
+
+<section class="about">
+    <article class="row text-center py-5">
+        <div class="col-12  pt-4">
+            <p class="podnaslov">always behind the brands</p>
+            <h5 class="py-2">A studio helping web 2.0 gain some momentum.</h5>
+            <p>The integration of web fonts has always been one of the largest contibuting
+                factors to
+                diversity in
+                the overal look and feel of websites today vs. yestrday.</p>
+        </div>
+    </article>
+    <article class="row p-5">
+        <div class="col-4">
+            <div class="wrapper">
+                <p><i class="fab fa-twitter"></i>&nbsp; Sync across all devices</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum accusamus porro quas doloribus in
+                    fugit ipsam, necessitatibus aliquam ab neque.</p>
+                <a class="link-red" href="">Read more</a>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="wrapper">
+                <p><i class="fab fa-twitter"></i>&nbsp; Sync across all devices</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum accusamus porro quas doloribus in
+                    fugit ipsam, necessitatibus aliquam ab neque.</p>
+                <a class="link-red" href="">Read more</a>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="wrapper">
+                <p><i class="fab fa-twitter"></i>&nbsp; Sync across all devices</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum accusamus porro quas doloribus in
+                    fugit ipsam, necessitatibus aliquam ab neque.</p>
+                <a class="link-red" href="">Read more</a>
+            </div>
+        </div>
+    </article>
+</section>
+
+<section class="team">
+    <article class="row naslov text-center py-5">
+        <div class="col-12">
+            <p class="podnaslov">always behind the brands</p>
+            <h2>Always creative team</h2>
+        </div>
+    </article>
+
+    <article class="row sadrzaj align-items-center justify-content-center container">
+        <div class="col-6 slika"></div>
+        <div class="col-6 podaci">
+            <h3>Lopez Onight</h3>
+            <h4>ceo</h4>
+            <p class="py-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus illum consequuntur numquam
+                voluptate
+                tempora, quis sequi. Sed aspernatur amet aliquid!</p>
+            <ul class="d-flex justify-content-between align-items-center">
+                <li><a href=""></a><i class="fab fa-facebook-square"></i></li>
+                <li><a href=""></a><i class="fab fa-instagram"></i></li>
+                <li><a href=""></a><i class="fab fa-github"></i></li>
+                <li><a href=""></a><i class="fab fa-facebook-square"></i></li>
+            </ul>
+        </div>
+    </article>
+
+    <article class="row sadrzaj align-items-center justify-content-center container">
+        <div class="col-6 slika"></div>
+        <div class="col-6 podaci">
+            <h3>Lopez Onight</h3>
+            <h4>ceo</h4>
+            <p class="py-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus illum consequuntur numquam
+                voluptate
+                tempora, quis sequi. Sed aspernatur amet aliquid!</p>
+            <ul class="d-flex justify-content-between align-items-center">
+                <li><a href=""></a><i class="fab fa-facebook-square"></i></li>
+                <li><a href=""></a><i class="fab fa-instagram"></i></li>
+                <li><a href=""></a><i class="fab fa-github"></i></li>
+                <li><a href=""></a><i class="fab fa-facebook-square"></i></li>
+            </ul>
+        </div>
+    </article>
+
+    <article class="row sadrzaj align-items-center justify-content-center container">
+        <div class="col-6 slika"></div>
+        <div class="col-6 podaci">
+            <h3>Lopez Onight</h3>
+            <h4>ceo</h4>
+            <p class="py-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus illum consequuntur numquam
+                voluptate
+                tempora, quis sequi. Sed aspernatur amet aliquid!</p>
+            <ul class="d-flex justify-content-between align-items-center">
+                <li><a href=""></a><i class="fab fa-facebook-square"></i></li>
+                <li><a href=""></a><i class="fab fa-instagram"></i></li>
+                <li><a href=""></a><i class="fab fa-github"></i></li>
+                <li><a href=""></a><i class="fab fa-facebook-square"></i></li>
+            </ul>
+        </div>
+    </article>
+</section>
+
+
+<?php include "partials/footer.php"?>
---
+++ partials/header.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Domaci petak flex</title>
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+        integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light container align-items-center justify-content-center">
+            <a class="navbar-brand" href="#">
+                <h3><span>S</span>ynx</h3>
+                <p>multipurpose</p>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Team</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Work</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Prices</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <section class="mainHeader">
+            <article class="text-center">
+                <div class="row">
+                    <div class="col-12 text-white">
+                        <h1 class="py-3">Most flexible one page html5 template</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, dolor. Deserunt dolores eum
+                            ipsam sit quas quae, modi est. Labore voluptates ex odit enim officia.</p>
+                        <button class="btn btn-danger">Features</button>
+                    </div>
+                </div>
+            </article>
+        </section>
+    </header>
